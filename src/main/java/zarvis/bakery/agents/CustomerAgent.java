26f076a7c37ca36d31ccfcec6766b1497be601d0
root <yassine.bouknify@gmail.com>
2017-11-17 12:45:05 +0000

    updtae of container

--- src/main/java/zarvis/bakery/agents/CustomerAgent.java
+++ src/main/java/zarvis/bakery/agents/CustomerAgent.java
@@ -2,6 +2,7 @@ package zarvis.bakery.agents;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
@@ -13,7 +14,7 @@ public class CustomerAgent extends Agent {
 	@Override
 	protected void setup() {
 		System.out.println("Hi I'm the agent Customer my name is:"+this.getAID().getName());
-		addBehaviour(new FindAllBackeryByNameBehaviour());			
+		addBehaviour(new FindAllBackeryByNameBehaviour(this,10000));			
 	}
 	
 	protected void takeDown() {
@@ -21,13 +22,18 @@ public class CustomerAgent extends Agent {
 		System.out.println("Agent "+getAID().getName()+" terminating.");
 	}
 //========================================================
-	class FindAllBackeryByNameBehaviour extends OneShotBehaviour{
+	class FindAllBackeryByNameBehaviour extends TickerBehaviour{
+		
+		public FindAllBackeryByNameBehaviour(Agent a, long period) {
+			super(a, period);
+		}
+
 		private AID[] backeryAgents;
 		
-		public void action() {
+		public void onTick() {
 			DFAgentDescription template = new DFAgentDescription();
 			ServiceDescription sd = new ServiceDescription();
-			sd.setType("backery");
+			sd.setType("bakery");
 			template.addServices(sd);
 			try {
 				DFAgentDescription[] result = DFService.search(myAgent, template);
@@ -37,6 +43,10 @@ public class CustomerAgent extends Agent {
 					backeryAgents[i] = result[i].getName();
 				}
 				System.out.println("Total number of backries found: " + backeryAgents.length);
+				if(backeryAgents.length > 0){
+					System.out.println("found backries");
+					doDelete();
+				}
 			} catch (FIPAException fe) {
 				fe.printStackTrace();
 			}
